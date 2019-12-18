2f225d69522adcc04ee4a270d8997d77ce1d9eb9
krunal3kapadiya <krunal3kapadiya@gmail.com>
2019-12-18 15:19:52 +0530

    stopwatch clock feature added

--- SmartClock/app/src/main/java/app/krunal3kapadiya/smartclock/ui/stopwatch/StopWatchFragment.kt
+++ SmartClock/app/src/main/java/app/krunal3kapadiya/smartclock/ui/stopwatch/StopWatchFragment.kt
@@ -1,18 +1,13 @@
 package app.krunal3kapadiya.smartclock.ui.stopwatch
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import app.krunal3kapadiya.smartclock.R
 import kotlinx.android.synthetic.main.fragment_stopwatch.*
-import java.util.concurrent.TimeUnit
 import kotlin.time.ExperimentalTime
 
 
@@ -24,13 +19,13 @@ import kotlin.time.ExperimentalTime
 
 class StopWatchFragment : Fragment() {
     companion object {
-        val BROADCAST_ACTION: String? = "app.krunal3kapadiya.smartclock.ui.BROADCAST"
-        val EXTENDED_DATA_STATUS: String? = "app.krunal3kapadiya.smartclock.ui.BROADCAST"
-
         fun newInstance(): StopWatchFragment {
             return StopWatchFragment()
         }
     }
+
+    private var pauseOffset: Long = 0L
+    private var running = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,42 +39,29 @@ class StopWatchFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val intent = Intent(context, StopWatchService::class.java)
-        var i = 0L
+        alarm_clock.base = SystemClock.elapsedRealtime()
+
         add_alarm_button.setOnClickListener {
-            // TODO start timer here
             if (add_alarm_button.isActivated) {
-                activity?.stopService(intent)
+                if (running) {
+                    alarm_clock.stop()
+                    pauseOffset = SystemClock.elapsedRealtime() - alarm_clock.base
+                    running = false
+                }
             } else {
-                activity?.startService(intent.putExtra(EXTENDED_DATA_STATUS, i))
+                if (!running) {
+                    alarm_clock.setBase(SystemClock.elapsedRealtime() - pauseOffset)
+                    alarm_clock.start()
+                    running = true
+                }
+
             }
             add_alarm_button.isActivated = !add_alarm_button.isActivated
         }
 
-        activity?.applicationContext?.let {
-            LocalBroadcastManager.getInstance(it).registerReceiver(
-                object : BroadcastReceiver() {
-                    override fun onReceive(context: Context, intent: Intent) {
-                        i = intent.getLongExtra(EXTENDED_DATA_STATUS, 0)
-                        alarm_clock.text = getFormattedNumbers(i)
-                    }
-                }, IntentFilter(BROADCAST_ACTION)
-            )
-        }
-
         text_reset.setOnClickListener {
-            i = 0
-            alarm_clock.text = getString(R.string._00_00_00)
+            alarm_clock.base = SystemClock.elapsedRealtime()
+            pauseOffset = 0
         }
-    }
-
-    fun getFormattedNumbers(number: Long): String {
-        // long hours = (milliseconds / 1000) / 60 / 60;
-        val hours = TimeUnit.MILLISECONDS.toHours(number)
-        // long minutes = (milliseconds / 1000) / 60;
-        val minutes = TimeUnit.MILLISECONDS.toMinutes(number)
-        // long seconds = (milliseconds / 1000);
-        val seconds = TimeUnit.MILLISECONDS.toSeconds(number)
-        return "$hours: $minutes : $seconds"
     }
 }
